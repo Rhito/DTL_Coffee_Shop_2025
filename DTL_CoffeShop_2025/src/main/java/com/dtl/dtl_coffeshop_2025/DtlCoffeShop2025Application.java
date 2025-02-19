f76c5fdf48eb4710adfa2774a2474ec26642e7c8
Rhito <lasnguyen06@gmail.com>
2025-02-19 09:32:41 +0700

    done db

---
+++ DTL_CoffeShop_2025/src/main/java/com/dtl/dtl_coffeshop_2025/DtlCoffeShop2025Application.java
@@ -0,0 +1,13 @@
+package com.dtl.dtl_coffeshop_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtlCoffeShop2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtlCoffeShop2025Application.class, args);
+    }
+
+}
---
+++ DTL_CoffeShop_2025/src/test/java/com/dtl/dtl_coffeshop_2025/DtlCoffeShop2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtl.dtl_coffeshop_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtlCoffeShop2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
